7d55751d27e8d466c1001c47d8a9f5727423a2a6
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-05-31 14:26:45 +0200

    controller suggestions ok

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,24 @@
 class SuggestionsController < ApplicationController
+  def new
+    @suggestion = Suggestion.new
+  end
+
+  def show
+    @suggestion = Suggestion.find(params[:id])
+  end
+
+  def create
+    @suggestion = Suggestion.new(suggestion_params(params))
+    if @suggestion.save
+      redirect_to @suggestion
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def suggestion_params(params)
+    params.require(:suggestion).permit(:title, :content, :votes, :address, :status, :user_id)
+  end
 end
