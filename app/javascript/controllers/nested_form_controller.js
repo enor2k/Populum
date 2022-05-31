7fcedb8d231133be508715ef8361ad1b10cfda53
Marc Delesalle <mrc.delesalle@gmail.com>
2022-05-31 17:32:43 +0200

    add question ok

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,15 +1,16 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["add_quesiton", "template"]
+  static targets = ["add_question", "template"]
 
-  add_association(event){
+  add_association(event) {
     event.preventDefault()
     const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, Math.floor(Math.random() * 20))
+    console.log(this.add_questionTarget)
     this.add_questionTarget.insertAdjacentHTML('beforebegin', content)
   }
 
-  remove_association(event){
+  remove_association(event) {
     event.preventDefault()
     let item = event.target.closest(".nested-fields")
     item.querySelector("input[name *= '_destroy']").value = 1
