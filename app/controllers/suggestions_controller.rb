6aebafdd566edd154746d25f525ff42230143129
enor de kerdanet <enor2k@gmail.com>
2022-05-31 14:42:15 +0200

    method done

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,22 @@
 class SuggestionsController < ApplicationController
+
+  def edit
+    @suggestion = Suggestion.find(params[:id])
+  end
+
+  def update
+    @suggestion = Suggestion.find(params[:id])
+    if @suggestion.update(suggestion_params)
+      redirect_to controller: :users_controller, action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @suggestion = Suggestion.find(params[:id])
+    @suggestion.destroy
+
+    redirect_to controller: :users_controller, action: :show
+  end
 end
