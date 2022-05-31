5923c968e1c98b54014c1064124b2206481ba5fc
Brice Pirioua <brice.pirioua@gmail.com>
2022-05-31 17:54:42 +0200

    fin conflits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :suggestions
+  resources :suggestions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :surveys, only: %i[new create index]
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   resources :suggestions, only: %i[new show create] do
     resources :comments, only: %i[create]
   end
-
 end
