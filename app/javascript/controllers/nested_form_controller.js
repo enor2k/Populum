deac954921e2dbf7b10957bc047ebc2ad6b0079a
Marc Delesalle <mrc.delesalle@gmail.com>
2022-06-01 15:37:27 +0200

    question-type

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,23 +1,3 @@
-// import { Controller } from "stimulus";
-
-// export default class extends Controller {
-//   static targets = ["add_question", "template"]
-
-//   add_association(event) {
-//     event.preventDefault()
-//     const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
-//     console.log(this.add_questionTarget)
-//     this.add_questionTarget.insertAdjacentHTML('beforebegin', content)
-//   }
-
-//   remove_association(event) {
-//     event.preventDefault()
-//     let item = event.target.closest(".nested-fields")
-//     item.querySelector("input[name *= '_destroy']").value = 1
-//     item.style.display = "none"
-//   }
-// }
-
 import { Controller } from "stimulus"
 
 export default class extends Controller {
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, allow_destroy: true
 
   enum question_type: { single_choice: 0, multiple_choice: 1, long_answer: 2 }
+
+  def self.question_type_select
+    question_types.keys.map { |k| [k.titleize, k] }
+  end
 end
