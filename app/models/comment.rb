b7283c52d5c014907b1fe39f7fceeeb18c32d00a
enor de kerdanet <enor2k@gmail.com>
2022-06-01 16:41:25 +0200

    seed done w/ Lorem ipsum suggestions and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :suggestion
   validates :content, presence: true
-  validates :content, length: { maximum: 200 }
+
 end
--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,5 +2,4 @@ class Suggestion < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, :content, :address, presence: true
-  validates :content, length: { minimum: 20, maximum: 400 }
 end
