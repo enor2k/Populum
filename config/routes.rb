e4307ca27976d2cc76c017bfeb78769a53cf0f8f
enor de kerdanet <enor2k@gmail.com>
2022-06-01 17:30:01 +0200

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  resources :suggestions, only: [:new, :show, :create, :update] do
-    resources :suggestion_votes, only: [:new, :create]
-=======
+  resources :suggestions, only: %i[new show create update] do
+    resources :suggestion_votes, only: %i[new create]
+  end
 
   resources :surveys, only: %i[new create index]
 
   resources :suggestions do
     resources :comments, only: %i[create]
->>>>>>> master
+
   end
 end
