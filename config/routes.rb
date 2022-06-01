74da48d2551efe41cc8e07d1f09896bb4c6cba27
enor de kerdanet <enor2k@gmail.com>
2022-06-01 12:25:11 +0200

    routes and validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :suggestions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :surveys, only: %i[new create index]
 
-  resources :suggestions, only: %i[new show create] do
+  resources :suggestions do
     resources :comments, only: %i[create]
   end
 end
