5470bb37c03ad9c51c786408e7c7e49e77a8e5a2
enor de kerdanet <enor2k@gmail.com>
2022-06-03 14:18:15 +0200

    user#show done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show]
 
   resources :suggestions do
     resources :suggestion_votes, only: %i[new create]
