a01c1fb33e5ee2eae68f6879fc4b940950b9816a
Marc Delesalle <mrc.delesalle@gmail.com>
2022-06-06 14:15:18 +0200

    pas de lien entre answers et visualisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,22 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def question_labels(question)
+    @question_labels = Option.where(question_id: question.id).map(&:title)
+    # byebug
+  end
+  helper_method :question_labels
+
+  def question_values(question)
+    @question_values = Answer.where(question_id: question.id).map { |a| Option.find(a.option_id).title }
+                      Answer.where(question_id:9)             .map{|a| Option.find(a.option_id).title}
+    # byebug
+  end
+  helper_method :question_values
+
+
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :avatar, :gender, :birthday])
--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -3,47 +3,85 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ["chart"]
-  static values = { keys: Array, values: Array}
+  static values = { keys: Array, values: Array, type: String}
 
   connect () {
-    console.log("hello from display controller")
-    console.log(this.keysValue)
-    console.log(this.valuesValue)
-    const ctx = this.chartTarget
-    console.log(ctx)
-    const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: this.keysValue,
-        datasets: [{
-            label: '# of Votes',
-            data: this.valuesValue,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      scales: {
-          y: {
-              beginAtZero: true
-          }
+    if (this.typeValue === 'single_choice') {
+      const ctx = this.chartTarget
+      console.log(ctx)
+      const myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          labels: this.keysValue,
+          datasets: [{
+              label: '# of Votes',
+              data: this.valuesValue,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
       }
+      });
     }
-    });
+
+    if (this.typeValue === 'multiple_choice') {
+      const ctx = this.chartTarget
+      console.log(ctx)
+      const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: this.keysValue,
+          datasets: [{
+              label: '# of Votes',
+              data: this.valuesValue,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+      }
+      });
+    }
   }
 }
