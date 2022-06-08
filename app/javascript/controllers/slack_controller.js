580dcd9d0d76b987ab322ebda1d6372c122a1eba
Marc Delesalle <mrc.delesalle@gmail.com>
2022-06-08 12:21:30 +0200

    nammespacing des fonctions du dashboard

--- app/javascript/controllers/slack_controller.js
+++ app/javascript/controllers/slack_controller.js
@@ -3,15 +3,15 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "showZone"]
 
-  focus(event) {
+//   focus(event) {
 
-    let url = window.location.href;
-    if (url.indexOf('?') > -1){
-      url = url.match(/.*dashboard\?/)
-      url += `&param=${event.currentTarget.dataset.id}`
-    }else{
-      url += `?&param=${event.currentTarget.dataset.id}`
-    }
-    window.location.href = url;
-  }
+//     let url = window.location.href;
+//     if (url.indexOf('?') > -1){
+//       url = url.match(/.*dashboard\?/)
+//       url += `&param=${event.currentTarget.dataset.id}`
+//     }else{
+//       url += `?&param=${event.currentTarget.dataset.id}`
+//     }
+//     window.location.href = url;
+//   }
 }
